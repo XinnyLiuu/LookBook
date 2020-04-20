6e3c6824d767246af6385199bed710bb2980e8a9
Xin Liu <xl4998@rit.edu>
2020-04-20 15:23:22 -0400

    test

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -168,6 +168,8 @@ class Recipes extends React.Component {
 	 * Grab all recipes 
 	 */
 	componentDidMount() {
+		console.log(process.env.REACT_APP_SERVER_IP);
+		
 		// Determine which view to load
 		if (this.props.type === "all") {
 			this.getRecipes();
